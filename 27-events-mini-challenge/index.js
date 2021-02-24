03fce121d661bf09d36a4a2c657c75ea7870d4ae
Michelle Rios <brendamichelle995@gmail.com>
2021-02-24 12:01:29 -0500

    after mini challenge review

--- 27-events-mini-challenge/index.js
+++ 27-events-mini-challenge/index.js
@@ -25,7 +25,7 @@ likes.textContent = `${traveler.likes} Likes`
 
 
 /***** Deliverable 4 *****/
-function renderAnimalSightingPost (animalObject) {
+function renderAnimalSightingPost(animalObject) {
     const li = document.createElement("li")
     li.dataset.id = animalObject.id
 
@@ -59,7 +59,25 @@ animalToRemove.remove()
 /************************** EVENTS JS MINI CHALLENGE ******************************/
 
 /***** Deliverable 1 *****/
-function toggleColor(element) {
+// function toggleColor(element) {
+//     if (element.style.color === "green") {
+//         element.style.color = "black"
+//     } else {
+//         element.style.color = "green"
+//     }
+// }
+
+// header.addEventListener('click', function (event) {
+//     // toggleColor(header)
+//     toggleColor(event.target) 
+//     //event.target tells which element triggered the event
+// })
+
+
+
+function toggleColor(event) {
+    const element = event.target
+
     if (element.style.color === "green") {
         element.style.color = "black"
     } else {
@@ -67,6 +85,54 @@ function toggleColor(element) {
     }
 }
 
+header.addEventListener('click', toggleColor)
+
+
 /***** Deliverable 2 *****/
 
+const likeButton = document.querySelector('.like-button')
+
+likeButton.addEventListener('click', function (event) {
+    // HOW TO GET P TAG THAT DISPLAYS LIKES NUMBER??
+    // - could use likes variable from line 23 to access likes p tag
+    // - const likesPtag = document.querySelector('p.likes')
+    // - const likePtag = event.target.previousElementSibling
+
+    // const currentLikes = parseInt(likes.textContent)
+    // likes.textContent = `${currentLikes + 1} Likes`
+
+    traveler.likes += 1
+    likes.textContent = `${traveler.likes} Likes`
+})
+
 /***** Deliverable 3 *****/
+
+
+const form = document.querySelector('#new-animal-sighting-form')
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    // const input1 = event.target[0].value
+    const speciesInput = event.target.species.value
+    const videoInput = event.target.link.value
+    const photoInput = event.target.photo.value
+    const descriptionInput = event.target.description.value
+    const length = traveler.animalSightings.length
+    const newId = traveler.animalSightings[length - 1].id + 1
+
+    const sightingObj = {
+        species: speciesInput,
+        photo: photoInput,
+        link: videoInput,
+        description: descriptionInput,
+        travelerId: 1,
+        id: newId
+    }
+
+    traveler.animalSightings.push(sightingObj)
+    console.log(traveler.animalSightings)
+    renderAnimalSightingPost(sightingObj)
+
+    event.target.reset()
+    // form.reset()
+})
