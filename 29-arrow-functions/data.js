baf555017287e0dd42191d004516ef5fb14bb200
Michelle Rios <brendamichelle995@gmail.com>
2021-02-24 16:01:33 -0500

    before arrow functions mini

---
+++ 29-arrow-functions/data.js
@@ -0,0 +1,36 @@
+const nums = [4, 66, 9, 77, 90, 10, 33, 1, 23]
+
+const books = [
+    {
+        title: "We",
+        author: "Zamyatin",
+        yearRead: 2015
+    },
+    {
+        title: "Omon Ra",
+        author: "Pelevin",
+        yearRead: 2017
+    },
+    {
+        title: "Notes From Underground",
+        author: "Dostoevsky",
+        yearRead: 2019
+    },
+    {
+        title: "The Defense",
+        author: "Nabakov",
+        yearRead: 2014
+    },
+    {
+        title: "War and Peace",
+        author: "Tolstoy",
+        yearRead: 2016
+    },
+    {
+        title: "Anna Karenina",
+        author: "Tolstoy",
+        yearRead: 2016
+    }
+]
+
+const bookTitles = ["We", "Omon Ra", "The Defense", "Pnin", "War and Peace", "The Foundation Pit", "Notes From Underground"]
---
+++ 29-arrow-functions/index.js
@@ -0,0 +1,37 @@
+// const doubleIt = num => num * 2
+
+// const createPerson = (name, age) => ({firstName: name, age: age})
+
+
+
+
+
+
+/********************** MAP **********************/
+// Transforms an array
+// Returns a new array
+// Does not mutate the original array
+// Callback should return the transformation
+
+
+
+
+/********************** FILTER **********************/
+// Returns a subarray satisfying a condition
+// Does not mutate the original array
+// Callback should return a boolean
+
+
+
+
+
+/********************** SORT **********************/
+// mutates the original array
+// callback should return a number
+
+
+
+
+
+
+
