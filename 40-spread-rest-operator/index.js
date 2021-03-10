761f84d9d08937a24e86c34743985998f5e9c254
Michelle Rios <brendamichelle995@gmail.com>
2021-03-10 11:43:31 -0500

    after lecture

--- 40-spread-rest-operator/index.js
+++ 40-spread-rest-operator/index.js
@@ -14,12 +14,27 @@ const pet = {
 }
 
 // Wrong way to make a copy of Raffy
+const notActualCopy = raffy
 
 // Correct (de-sugared) way to make a copy - using Object.assign
+const raffyCopy = Object.assign({}, raffy)
 
 // Correct (sugared) way using spread operator
+const anotherCopy = { ...raffy } // es6!
 
-// Combining raffy & pet objects
+// Combining raffy & pet objec
+const comboObj = Object.assign({}, raffy, pet)
+const anotherComboObj = { ...raffy, ...pet, favoriteFood: 'tacos' }
+
+
+
+
+
+
+
+
+
+
 
 
 /********************** What about arrays?? **********************/
@@ -28,10 +43,35 @@ const redVeggies = ["red cabbage", "red onion", "rhubarb"]
 const greenVeggies = ["spinach", "lettuce", "zucchini"]
 
 // Wrong way to make a copy of redVeggies
+const aFakeCopy = redVeggies
 
 // Correct (sugared) way using spread operator
+const actualArrCopy = [...redVeggies]
 
 // Combining redVeggies & greenVeggies arrays
+const allVeggies = [...redVeggies, ...greenVeggies]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -44,6 +84,36 @@ function sumThreeNums(x, y, z) {
 
 const arr = [3, 5, 7]
 
+// const sum = sumThreeNums(3, 5, 7)
+// const sum = sumThreeNums(arr[0], arr[1], arr[2])
+
+// const sum = sumThreeNums(...arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /********************** Rest Parameter **********************/
 /* 
@@ -52,7 +122,10 @@ const arr = [3, 5, 7]
  into the function
 - Must be the last paramter 
 */
-function dynamicSum(...moreArgs) {
+function dynamicSum(beef, chicken, ...moreArgs) {
+    console.log(beef)
+    console.log(chicken)
+    console.log(moreArgs)
     return moreArgs.reduce((acc, currVal) => {
         return acc + currVal
     }, 0)
@@ -62,13 +135,22 @@ function dynamicSum(...moreArgs) {
 // console.log(sum)
 
 
-/********************** Rest With Destructuring **********************/
-const nums = [0, 1, 2, 3, 4]
+
+
+
+
+/********************** Rest Operator With Destructuring **********************/
+const nums = [0, 1, 2, 3, 4, 5, 6, 7]
 
 const spring = {
     temperature: 'temperate',
     activities: ['running', 'walks', 'hiking', 'gardening'],
     isDaylightSaving: true
 }
+
+
+const [num1, num2, num3, num4, ...beef] = nums
+
+const { temperature, ...newObj } = spring
 
 
