a8151ad648d41d5281bcdaa28f28db1d1c11a946
ihollander <ianholla@gmail.com>
2021-03-17 14:28:10 -0400

    After forms

--- 45-react-forms/project-showcase/src/components/App.js
+++ 45-react-forms/project-showcase/src/components/App.js
@@ -4,18 +4,34 @@ import ProjectForm from "./ProjectForm";
 import ProjectList from "./ProjectList";
 
 // TODO: replace this with a fetch request (eventually)
-import projectData from "../data/projects";
+// import projectData from "../data/projects";
 
 function App() {
-  const [projects, setProjects] = useState(projectData);
+  const [projects, setProjects] = useState([]); // {}, null, empty array
   const [isDarkMode, setIsDarkMode] = useState(true);
 
   function handleToggleDarkMode() {
     setIsDarkMode(!isDarkMode);
   }
 
+  function handleAddProject(newProject) {
+    const newProjects = [...projects, newProject];
+    setProjects(newProjects);
+
+    // this won't work :(
+    // projects.push(newProject)
+    // setProjects(projects)
+  }
+
+  // { image: "http://" }
+  // image
   function handleFetchProjects() {
     // TODO: GET /projects
+    fetch("http://localhost:4000/projects")
+      .then((r) => r.json())
+      .then((projectArray) => {
+        setProjects(projectArray);
+      });
   }
 
   return (
@@ -25,9 +41,12 @@ function App() {
         isDarkMode={isDarkMode}
         onToggleDarkMode={handleToggleDarkMode}
       />
-      <ProjectForm />
+
+      <ProjectForm onAddProject={handleAddProject} />
+
       <button onClick={handleFetchProjects}>Fetch Projects</button>
       <ProjectList projects={projects} />
+      {/* {projects ? <ProjectList projects={projects} /> : <h1>Loading...</h1>} */}
     </div>
   );
 }
--- 45-react-forms/project-showcase/src/components/ProjectForm.js
+++ 45-react-forms/project-showcase/src/components/ProjectForm.js
@@ -1,19 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ProjectForm() {
+// ✅ form submit event
+// ✅ preventDefault
+// event.target (get the input field data)
+// - keep some state updated any time the inputs change!
+// ✅ POST to save the data to the database
+// update the DOM with the new whatever we created
+// - update state
+
+function ProjectForm({ onAddProject }) {
+  const [name, setName] = useState("");
+  const [about, setAbout] = useState("");
+  const [phase, setPhase] = useState("1");
+  const [link, setLink] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // POST /projects
+    // body: { name, about, phase, link, image }
+    const formData = {
+      name: name,
+      about: about,
+      phase: parseInt(phase),
+      link: link,
+      image: image,
+    };
+    fetch("http://localhost:4000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((actualNewProject) => {
+        onAddProject(actualNewProject);
+      });
+  }
+
   return (
     <section>
-      <form className="form" autoComplete="off">
+      <form onSubmit={handleSubmit} className="form" autoComplete="off">
         <h3>Add New Project</h3>
 
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {name.length === 0 ? (
+          <p style={{ color: "red" }}>You must input a name!</p>
+        ) : null}
 
         <label htmlFor="about">About</label>
-        <textarea id="about" name="about" />
+        <textarea
+          id="about"
+          name="about"
+          value={about}
+          onChange={(e) => setAbout(e.target.value)}
+        />
 
         <label htmlFor="phase">Phase</label>
-        <select name="phase" id="phase">
+        <select
+          name="phase"
+          id="phase"
+          value={phase}
+          onChange={(e) => setPhase(e.target.value)}
+        >
           <option value="1">Phase 1</option>
           <option value="2">Phase 2</option>
           <option value="3">Phase 3</option>
@@ -22,10 +79,22 @@ function ProjectForm() {
         </select>
 
         <label htmlFor="link">Project Homepage</label>
-        <input type="text" id="link" name="link" />
+        <input
+          type="text"
+          id="link"
+          name="link"
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+        />
 
         <label htmlFor="image">Screenshot</label>
-        <input type="text" id="image" name="image" />
+        <input
+          type="text"
+          id="image"
+          name="image"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
 
         <button type="submit">Add Project</button>
       </form>
